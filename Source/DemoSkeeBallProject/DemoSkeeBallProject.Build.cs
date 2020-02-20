f28591bf04c404bce4f4e5c6986eceb0ded61f8b
JoelYatesCollege <59893600+JoelYatesCollege@users.noreply.github.com>
2020-02-20 15:01:21 -0500

    added Config and Source files

---
+++ Source/DemoSkeeBallProject/DemoSkeeBallProject.Build.cs
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class DemoSkeeBallProject : ModuleRules
+{
+	public DemoSkeeBallProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "ProceduralMeshComponent" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
+
+		PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "VRBase")));
+	}
+}
